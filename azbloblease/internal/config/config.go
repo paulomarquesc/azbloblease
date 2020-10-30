7a3bb8257ffb2d1642824113ff6f6f13c6aee618
Paulo Marques da Costa <pmarques@microsoft.com>
2020-10-30 13:08:29 -0700

    version bump

--- azbloblease/internal/config/config.go
+++ azbloblease/internal/config/config.go
@@ -12,7 +12,7 @@ import (
 
 // Constants
 const (
-	version              = "0.1.0"
+	version              = "1.0.0"
 	blobName             = "azblobleaseblob"
 	success              = "Success"
 	fail                 = "Fail"
